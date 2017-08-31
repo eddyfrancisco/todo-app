6a851f6d7c14ceaf1d900bef22db1ff93fe922fe
eddyfrancisco <eddy.pegasus@gmail.com>
2017-08-31 20:13:03 -0300

    First commit!

---
+++ backend/src/config/server.js
@@ -0,0 +1,18 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const cors = require('cors')
+
+server.use(bodyParser.urlencoded({ extended: true })) 
+server.use(bodyParser.json())
+
+server.use(cors({origin: '*'}));
+server.set('secret', 'opensecret');
+
+server.listen(port, function(){
+   console.log(`BACKEND  is running on port ${port}.`)
+})
+
+module.exports = server
