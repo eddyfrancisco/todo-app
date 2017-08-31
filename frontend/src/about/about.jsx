5c0eb0bf8305286617662e35748bcf2f24d20515
eddyfrancisco <eddy.pegasus@gmail.com>
2017-08-31 20:16:11 -0300

    Second commit..

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa história</h2>
+        <p>A nossa história começa a muito tempo atrás..</p>
+        <h2>Missão e visão</h2>
+        <p>Nossa missão é sempre buscar e aprimorar...</p>
+        <h2>Imprensa</h2>
+        <p>Fomos destaque em alguns meios de comunicação..</p>
+         
+    </div>    
+)
